0d11f13eb20bb7a13fe53b46a058215c7a2cf69f
jgustavoj <gjime011@fiu.edu>
2021-03-11 19:41:51 -0500

    key

--- btre/settings.py
+++ btre/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yzz9(5^omri$7fzm0f21vhp8m^r105o+&e=k8$rhqo^&*1ze_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
